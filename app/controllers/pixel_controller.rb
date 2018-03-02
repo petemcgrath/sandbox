3bbad13c47004386c38a8f89511fd247e682158c
Pete McGrath <pete.j.mcgrath@gmail.com>
2018-03-02 18:18:20 -0500

    Render routes as son for now

--- app/controllers/pixel_controller.rb
+++ app/controllers/pixel_controller.rb
@@ -1,20 +1,24 @@
 class PixelController < ApplicationController
 
+  def index
+    render json: Pixel.all
+  end
+
   def inc
     pixel = Pixel.find_by_id(params[:pixel_id])
     if pixel.present?
       pixel.count += 1
       pixel.save!
-
-      @count = pixel.count
     end
+
+    render json: pixel.present? ? pixel.count : nil
   end
 
   def create_new_pixel
     pixel = Pixel.find_or_create_by(id: params[:pixel_id])
     @count = pixel.count
 
-    render :inc
+    render json: pixel.present? ? pixel.count : nil
   end
 
 end
